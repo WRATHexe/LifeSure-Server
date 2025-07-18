29611146404ef19acf649c4655b18c5d4e8f3015
WRATHeXe <FardinRuhian001@gmail.com>
2025-07-18 23:37:33 +0600

    Update .gitignore to include Vercel configuration files and comment out MongoDB connection logs

--- index.js
+++ index.js
@@ -174,7 +174,7 @@ const verifyCustomer = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
     
     // Get the database
     const db = client.db("LifeSureDB");
@@ -194,8 +194,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     // Basic route
     app.get('/', (req, res) => {
